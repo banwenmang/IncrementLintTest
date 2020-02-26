c7a56aa9512dd10e36ae71a63fc420e96542be00
zhoujx <zhoujx@guahao.com>
2020-02-26 09:40:50 +0800

    commit2

---
+++ app/src/main/java/com/wenmang/incrementlinttest/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.wenmang.incrementlinttest;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * desc:
+ * author: created by zhoujx on 2020/2/26 09:38
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
